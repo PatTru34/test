cf56fc6713ab13045e67eec37c159fb495e6266e
Pat.Tru34 <162391@student.uwm.edu.pl>
2023-10-19 15:10:03 +0200

    Lab3

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from .models import Person,Team
+from .models import Person,Team,Osoba,Stanowisko
+
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ['imie', 'nazwisko','plec','idstan']
+    list_filter = ['stanowisko']
+
+    @admin.display(description="id")
+    def idstan(self,obj):
+        return f"{obj.stanowisko} ({obj.stanowisko.id})"
+
 
 admin.site.register(Person)
 admin.site.register(Team)
-
+admin.site.register(Stanowisko)
+admin.site.register(Osoba, OsobaAdmin)
 
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
+MONTHS = models.IntegerChoices('Miesiace',
+                               'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
 
 SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-    )
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+)
+
+
 class Team(models.Model):
     name = models.CharField(max_length=60)
     country = models.CharField(max_length=2)
@@ -16,11 +19,33 @@ class Team(models.Model):
 
 
 class Person(models.Model):
-
     name = models.CharField(max_length=60)
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
     month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
 
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=60, null=False, blank=False)
+    opis = models.CharField(max_length=60, null=True, blank=True)
+
     def __str__(self):
-        return self.name
+        return f"{self.nazwa}"
+
+
+class Osoba(models.Model):
+    PLEC = (
+        ('M', 'Mezczyzna'),
+        ('K', 'Kobieta'),
+        ('I', 'Inne'),
+    )
+    imie = models.CharField(max_length=60, null=False, blank=False)
+    nazwisko = models.CharField(max_length=60, null=False, blank=False)
+    plec = models.CharField(max_length=1, choices=PLEC)
+    stanowisko = models.ForeignKey(Stanowisko, on_delete=models.CASCADE)
+    data_dodania = models.DateField(null=True, auto_now_add=True)
+
+    class Meta:
+        ordering = ["nazwisko"]
+    def __str__(self):
+        return self.imie+ " " +self.nazwisko
